6419394ebc885cbada7e4b47756ac9a6dfd0dac7
abdulsuleman7 <abdulsuleman7183@gmail.com>
2025-09-01 23:30:19 +0530

    added comments

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-
+// tags of html storing in the variable using the DOM()
 let input = document.querySelector("#input")
 let task_list = document.querySelector("#task_list")
 let btn = document.querySelector("#btn")
 let btn2 = document.querySelector("#btn2")
 
+// click event on the button adding the task..
 btn.addEventListener("click", () => {
     let val = input.value;
     if (val === "") {
         alert("Enter The Task")
     }
 
+    // list is created and the task is stored in this list
     let a = document.createElement("li")
     a.classList.add("list")
 
-
+// the new button 3  is created for the removing the specific task from the list and some styles to the button 3
     let b = document.createElement("button")
     b.classList.add("btn3")
     b.style.backgroundColor = "white"
@@ -22,20 +24,24 @@ btn.addEventListener("click", () => {
     b.style.marginLeft = "20px"
     b.style.fontSize = "20px"
 
+    // the button 3 will functions on clicking , remove the task 
     b.addEventListener("click",()=>{
         alert("Task Will Delete Permanently.....")
         a.remove();
         b.remove();
     })
-
+//  the task will added in the list..
     a.innerHTML = val ;
 
+    
     task_list.append(a)
     a.appendChild(b)
     // task_list.append(b)
     input.value = "";
 })
 
+
+// the button 2 works for removing the all task from the from the list..
 btn2.addEventListener("click", () => {
     task_list.innerHTML = "";
 })
